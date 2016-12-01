869109d970fc39847767430d0e27fc9a2c3cfe3b
Luebeck.jonathan <Luebeck.jonathan@B201PC715.berufskolleg-aachen.de>
2016-12-01 11:06:23 +0100

    main development

--- 05_LinieZeichnen_v01_korrigiert/src/JfLeinwand.java
+++ 05_LinieZeichnen_v01_korrigiert/src/JfLeinwand.java
@@ -6,30 +6,18 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 public class JfLeinwand extends JFrame {
 	/**
 	 * Schiffe Versenken
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	WcPoligon auto = new WcPoligon();
-	WcKreis reifen1 = new WcKreis(new WcPoint(0.3, 0.6), new WcDistance(0.2),getContentPane().getBackground(),Color.black);
-	WcKreis reifen2 = new WcKreis(new WcPoint(0.7, 0.6), new WcDistance(0.2),getContentPane().getBackground(),Color.black);
-	
-	
-
-	WcProgressBar bar;
-	JSlider sl = new JSlider();
 
 	ArrayList<WcGraphics> list = new ArrayList<WcGraphics>();
-
-	public static int cpw;
-
+	
+	Spielfeld spielfeld = new Spielfeld();
+	
 	private JPanel contentPane;
 
 	/**
@@ -60,20 +48,10 @@ public class JfLeinwand extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		auto.addPoint(0.15,0.60);
-		auto.addPoint(0.20,0.40);
-		auto.addPoint(0.30,0.40);
-		auto.addPoint(0.45,0.20);
-		auto.addPoint(0.65,0.20);
-		auto.addPoint(0.70,0.40);
-		auto.addPoint(0.85,0.40);
-		auto.addPoint(0.85,0.60);
-		
-		list.add(auto);
-		list.add(reifen1);
-		list.add(reifen2);
-		
+
+
+		list.add(spielfeld);
+
 		// modifizierte JLabel-Klasse ------------------------------------------
 		JLabel lbl_malen = new JLabel() {
 
@@ -84,17 +62,16 @@ public class JfLeinwand extends JFrame {
 
 			@Override
 			protected void paintComponent(Graphics g) {
-				
+
 				super.paintComponent(g);
-				JfLeinwand.cpw = contentPane.getWidth();
-				
+
 				// 1) rhh: Label an Fenstergröße anpassen
-				setBounds(0, 100, contentPane.getWidth(), contentPane.getHeight() - 100); 
-				
+				setBounds(0, 0, contentPane.getWidth(), contentPane.getHeight());
+
 				// 2) rhh: Labelgröße an WcPoint-Klasse zur Umrechnung
 				// weiterleiten
-				WcPoint.maxPixelX = this.getWidth();
-				WcPoint.maxPixelY = this.getHeight();
+				WcPoint.maxPixelX = this.getWidth() - 1; // -1 to improve
+				WcPoint.maxPixelY = this.getHeight() - 1; // alignment
 
 				g.setColor(Color.black);
 				for (WcGraphics graphic : list) {
@@ -105,27 +82,5 @@ public class JfLeinwand extends JFrame {
 		// --------------------------------------------------------------------
 		lbl_malen.setBounds(0, 0, 1, 1);
 		contentPane.add(lbl_malen);
-
-		bar = new WcProgressBar(contentPane);
-
-		bar.setBounds(0, 0, 343, 100);
-
-		contentPane.add(bar);
-
-		sl.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent arg0) {
-				dochange();
-			}
-		});
-
-		contentPane.add(sl);
-		sl.setSize(484, 31);
-		sl.setLocation(0, 100);
-
-	}
-
-	public void dochange() {
-		bar.setProgress(sl.getValue());
-		repaint();
 	}
 }
